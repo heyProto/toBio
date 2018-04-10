515d5d76ef716f4af91e5069e33c98ad37ed83d0
Amit Badheka <ab@pykih.com>
2018-04-10 10:57:04 +0530

    Fixes

--- src/js/Container.jsx
+++ src/js/Container.jsx
@@ -50,26 +50,34 @@ export default class toBioCard extends React.Component {
       )
     }else{
       let name = this.state.dataJSON.card_data.data.name,
-        designation = this.state.dataJSON.card_data.data.designation,
+        full_designation = this.state.dataJSON.card_data.data.designation,
         image = this.state.dataJSON.card_data.data.image_url === '' ? 'default.png' : this.state.dataJSON.card_data.data.image_url,
         description = this.state.dataJSON.card_data.data.description,
         lin_img = this.state.dataJSON.card_data.data.linkedin_url,
         fb_img = this.state.dataJSON.card_data.data.facebook_url,
-        tw_img = this.state.dataJSON.card_data.data.twitter_url;
+        tw_img = this.state.dataJSON.card_data.data.twitter_url,
+        designation, show_tooltip;
+        if (full_designation && full_designation.length > 65){
+          designation = full_designation.substring(0, 62) + "..."
+          show_tooltip = true
+        } else {
+          designation = full_designation;
+          show_tooltip = false
+        }
       return(
         <div className="col-7">
           <div className="bio-card">
             <div className="profile-image"><img src={image} height="60px"/></div>
             <div className="profile-info">
               <div className="name">{name}</div>
-              <div className="designation">{designation}</div>
+              <div className="designation" title={show_tooltip ? full_designation : undefined }>{designation}</div>
               <div className="social-share-icons">
-                {lin_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
-                {tw_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
-                {fb_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
+                {(lin_img && lin_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
+                {(tw_img && tw_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
+                {(fb_img && fb_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
               </div>
             </div>
-            {description !== '' ? <div className="bio-text">{description}</div> : ''}
+            {description && description !== '' ? <div className="bio-text">{description}</div> : ''}
           </div>
         </div>
       )
@@ -83,23 +91,31 @@ export default class toBioCard extends React.Component {
       )
     }else{
       let name = this.state.dataJSON.card_data.data.name,
-        designation = this.state.dataJSON.card_data.data.designation,
+        full_designation = this.state.dataJSON.card_data.data.designation,
         image = this.state.dataJSON.card_data.data.image_url === '' ? 'default.png' : this.state.dataJSON.card_data.data.image_url,
         description = this.state.dataJSON.card_data.data.description,
         lin_img = this.state.dataJSON.card_data.data.linkedin_url,
         fb_img = this.state.dataJSON.card_data.data.facebook_url,
-        tw_img = this.state.dataJSON.card_data.data.twitter_url;
+        tw_img = this.state.dataJSON.card_data.data.twitter_url,
+        designation, show_tooltip;
+        if (full_designation && full_designation.length > 65){
+          designation = full_designation.substring(0, 62) + "..."
+          show_tooltip = true
+        } else {
+          designation = full_designation;
+          show_tooltip = false
+        }
       return(
         <div className="col-4">
           <div className="bio-card">
             <div className="profile-image"><img src={image} height="60px"/></div>
             <div className="profile-info">
               <div className="name">{name}</div>
-              <div className="designation">{designation}</div>
+              <div className="designation" title={show_tooltip ? full_designation : undefined }>{designation}</div>
               <div className="social-share-icons">
-                {lin_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
-                {tw_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
-                {fb_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
+                {(lin_img && lin_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
+                {(tw_img && tw_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
+                {(fb_img && fb_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
               </div>
             </div>
             {description !== '' ? <div className="bio-text">{description}</div> : ''}
@@ -116,23 +132,31 @@ export default class toBioCard extends React.Component {
       )
     }else{
       let name = this.state.dataJSON.card_data.data.name,
-        designation = this.state.dataJSON.card_data.data.designation,
+        full_designation = this.state.dataJSON.card_data.data.designation,
         image = this.state.dataJSON.card_data.data.image_url === '' ? 'default.png' : this.state.dataJSON.card_data.data.image_url,
         description = this.state.dataJSON.card_data.data.description,
         lin_img = this.state.dataJSON.card_data.data.linkedin_url,
         fb_img = this.state.dataJSON.card_data.data.facebook_url,
-        tw_img = this.state.dataJSON.card_data.data.twitter_url;
+        tw_img = this.state.dataJSON.card_data.data.twitter_url,
+        designation, show_tooltip;
+        if (full_designation && full_designation.length > 50){
+          designation = full_designation.substring(0, 47) + "..."
+          show_tooltip = true
+        } else {
+          designation = full_designation;
+          show_tooltip = false
+        }
       return(
         <div className="col-3">
           <div className="bio-card">
             <div className="profile-image"><img src={image} height="60px"/></div>
             <div className="profile-info">
               <div className="name">{name}</div>
-              <div className="designation">{designation}</div>
+              <div className="designation" title={show_tooltip ? full_designation : undefined }>{designation}</div>
               <div className="social-share-icons">
-                {lin_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
-                {tw_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
-                {fb_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
+                {(lin_img && lin_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
+                {(tw_img && tw_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
+                {(fb_img && fb_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
               </div>
             </div>
             {description !== '' ? <div className="bio-text">{description}</div> : ''}
@@ -150,23 +174,31 @@ export default class toBioCard extends React.Component {
       )
     }else{
       let name = this.state.dataJSON.card_data.data.name,
-        designation = this.state.dataJSON.card_data.data.designation,
+        full_designation = this.state.dataJSON.card_data.data.designation,
         image = this.state.dataJSON.card_data.data.image_url === '' ? 'default.png' : this.state.dataJSON.card_data.data.image_url,
         description = this.state.dataJSON.card_data.data.description,
         lin_img = this.state.dataJSON.card_data.data.linkedin_url,
         fb_img = this.state.dataJSON.card_data.data.facebook_url,
-        tw_img = this.state.dataJSON.card_data.data.twitter_url;
+        tw_img = this.state.dataJSON.card_data.data.twitter_url,
+        designation, show_tooltip;
+        if (full_designation && full_designation.length > 48){
+          designation = full_designation.substring(0, 45) + "..."
+          show_tooltip = true
+        } else {
+          designation = full_designation;
+          show_tooltip = false
+        }
       return(
         <div className="col-2">
           <div className="bio-card">
             <div className="profile-image"><img src={image} height="60px"/></div>
             <div className="profile-info">
               <div className="name">{name}</div>
-              <div className="designation">{designation}</div>
+              <div className="designation" title={show_tooltip ? full_designation : undefined }>{designation}</div>
               <div className="social-share-icons">
-                {lin_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
-                {tw_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
-                {fb_img !== '' ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
+                {(lin_img && lin_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/lin.png" height="12px"/> : ''}
+                {(tw_img && tw_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/tw.png" height="12px"/> : ''}
+                {(fb_img && fb_img !== '') ? <img src="https://cdn.protograph.pykih.com/Assets/fb.png" height="12px"/> : ''}
               </div>
             </div>
             {description !== '' ? <div className="bio-text">{description}</div> : ''}
